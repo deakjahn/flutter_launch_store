ab771976d75549fd50af34da10df49c3683b518b
deakjahn <deakjahn@gmail.com>
2023-05-12 01:32:07 +0200

    Support ".huawei" in app id.

--- lib/flutter_launch_store.dart
+++ lib/flutter_launch_store.dart
@@ -3,7 +3,7 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class StoreLauncher {
-  static const MethodChannel _channel = const MethodChannel('flutter_launch_store');
+  static const MethodChannel _channel = MethodChannel('flutter_launch_store');
 
   static Future<void> openWithStore(String appId) async {
     await _channel.invokeMethod('openWithStore', {"app_id": appId});
